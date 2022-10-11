8bff198db46f6dc97c04bb3c912e9d08be39ed27
AnastasiyaLytvyn <n8stusikk@gmail.com>
2022-10-11 20:52:35 +0300

    add second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector(".gallery");
+const itemsMarkUp = createGalleryItemsMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML("beforeend", itemsMarkUp);
+paletteContainer.addEventListener("click", onImageClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+
+function onImageClick(event) {
+    event.preventDefault();
+    if(event.target.classList.contains('.gallery__image')){
+        return;
+    }
+}
+
+const lightBox = new SimpleLightbox('.gallery a', { captionsData:"alt", captionDelay:250 });
